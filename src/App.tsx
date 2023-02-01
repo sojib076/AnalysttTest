73a98677c7f88eefc4c2646773194e09eac049b0
sojib076 <sojibds332@gmail.com>
2023-02-01 16:13:44 +0600

    userAndBasic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './Components/Home/Home';
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router';
+import Details from './Components/Details/Details';
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      path: '/', element: <Home />
+    },
+    {
+      path: `/details/:id`,  
+      
+      loader: ({params})=> fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`).then(res=>res.json()),
+      element: <Details />
+    }
+  ])
   return (
     <div className="App">
-      
+      <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
+    const [users, setUsers] = useState<any[]>([])
+// write a async function to fetch data from the server
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+            setUsers(data);
+        }
+        fetchUsers();
+    }, []);
+   console.log(users);
     return (
-        <div>
-            
+        <div className='w-[90%] mx-auto mt-10'>
+            {
+                users?.map(user => {
+                    return (
+                        
+                            <div className='grid grid-cols-5 h-[150px]  shadow-2xl my-10  justify-center items-center '>  
+                                <div className='mt-5'>UserName </div>
+                                <div className='flex flex-col '> <span className='font-bold'> Contract </span>  {user.name}</div>
+                                <div className='flex flex-col'> <span className='font-bold'> City </span>  {user.address.city}</div>
+                                <div className='flex flex-col'> <span className='font-bold'> State </span>  {user.address.street}</div>
+                                <Link to={`/details/${user.id}`} className='bg-red-500 text-white p-2 rounded-md'>More Info</Link>
+                            
+                            </div>
+                      
+                    )
+                })
+            }
         </div>
     );
 };
